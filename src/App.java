1fb53cb3bb7d906ae40fa34232ad18b681a8b2d8
Taisiya <lebei@mail.ru>
2023-08-02 13:48:44 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList list = new LinkedList();
+        list.add(3);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+
+        list.print();
+        list.reverse();
+        list.print();
+
+    }
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,69 @@
+
+public class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        Node currentNode = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+
+    }
+
+    public void remove(int data) {
+        Node currentNode = head;
+        Node previousNode = null;
+        while (currentNode.next != null) {
+            if (currentNode.data == data)
+                if (currentNode == head) {
+                    head = currentNode.next;
+                }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void print() {
+        Node currentNode = head;
+        if (head != null) {
+            System.out.println(head.data);
+        }
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+            System.out.println(currentNode.data);
+        }
+    }
+
+    public void reverse() {
+        Node currentNode = head;
+        Node previousNode = null;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = next;
+        }
+        head = previousNode;
+    }
+
+}
